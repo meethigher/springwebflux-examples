e74f9744a4c7c10ff2946ef08a9155214ac47fb1
meethigher <meethigher@qq.com>
2024-12-21 22:42:09 +0800

    feat: 分模块，便于学习

---
+++ README.md
@@ -0,0 +1,7 @@
+项目结构说明
+
+```
+├─springboot-webflux: 使用springboot+springwebflux实现的web程序
+├─reactor: 主要学习reactor中的Mono与Flux
+```
+
---
+++ reactor/src/main/java/top/meethigher/Main.java
@@ -0,0 +1,7 @@
+package top.meethigher;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
